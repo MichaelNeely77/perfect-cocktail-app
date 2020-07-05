461f3e4cae150147bf91a2dc6d60f01f34d62684
Michael Neely <michael@michaelneely.net>
2020-07-05 16:21:26 -0400

    Added the alert for blank search form on the Cocktails name page

--- js/UI.js
+++ js/UI.js
@@ -1,8 +1,23 @@
 class UI {
     // Displays a cutom message
 
-    printMessage() {
-        console.log('From the method');
+    printMessage(message, className) {
+        const div = document.createElement('div');
+
+        div.innerHTML = `
+            <div class="alert alert-dismissable alert-${className}">
+                <button type="button" class="close" data-dismiss="alert">X</button>
+                ${message}
+            </div>
+        `;
+
+        const reference = document.querySelector('.jumbotron h1');
+        const parentNode = reference.parentElement;
+        parentNode.insertBefore(div, reference);
+
+        setTimeout(() => {
+            document.querySelector('.alert').remove();
+        }, 3000)
     }
 
 }
--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ function getCocktails(e) {
     const searchTerm = document.querySelector('#search').value;
 
     if(searchTerm === '') {
-        ui.printMessage();
+        ui.printMessage('Please add somthing into the form', 'danger');
     } else {
         console.log('Query the REST API');
     }
